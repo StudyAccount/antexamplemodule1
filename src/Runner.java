1999d845983133893b1ce6c9e6b548ece88c7a7d
SleepWalker <vityagorbenko@yandex.ru>
2016-07-31 00:28:15 +0300

    init

---
+++ src/Runner.java
@@ -0,0 +1,16 @@
+import output.Output;
+
+/**
+ * Created by SleepWalker on 06.06.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+
+
+        Output.outputWriter(10000);
+        Output.outputWriter(100000);
+        Output.outputWriter(1000000);
+
+
+    }
+}
